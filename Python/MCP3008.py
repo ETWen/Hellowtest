90b22fb8798efacca6ff9130fc6e27f86d032eed
ETWen <eric4411@gmail.com>
2018-04-17 15:31:25 +0000

    The serial has bugs.

--- Python/MCP3008.py
+++ Python/MCP3008.py
@@ -3,13 +3,13 @@ import time
 
 ch_num=4
 
-cs_pin=29
-clk_pin=31
+cs_pin=35
+clk_pin=29
 mosi_pin=33
-miso_pin=35		#Data in
+miso_pin=31		#Data in
 
-t_SUCS=100e-9
-t_SU=50e-9
+#t_SUCS=100e-9
+#t_SU=50e-9
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -25,28 +25,26 @@ if (ch_num>7 || ch_num<1)
 '''
 try:
         while True:
-                
+                sum=0
                 GPIO.output(cs_pin,GPIO.HIGH)
                 GPIO.output(clk_pin,GPIO.LOW)
                 GPIO.output(cs_pin,GPIO.LOW)
 
                 for i in range(0,5,1):
-                        GPIO.output(clk_pin,GPIO.HIGH)
-                        GPIO.output(clk_pin,GPIO.LOW)
-                        
                         GPIO.output(mosi_pin,GPIO.HIGH)   #start
-                        GPIO.output(mosi_pin,GPIO.HIGH)   #SGL/DIFF
-                        GPIO.output(mosi_pin,GPIO.LOW)    #0,0,0 Channel0
+                        GPIO.output(clk_pin,GPIO.HIGH)
                         GPIO.output(mosi_pin,GPIO.LOW)
-                        GPIO.output(mosi_pin,GPIO.LOW)
-                
+                        GPIO.output(clk_pin,GPIO.LOW)
+
                 for j in range(0,12,1):
                         GPIO.output(clk_pin,GPIO.HIGH)
                         GPIO.output(clk_pin,GPIO.LOW)
-                        
                         adcout=GPIO.input(miso_pin)
-                
 
+                        '''
+                        if(j>1):
+                                sum+=adcout*(2**(j-1))
+                        '''
                 GPIO.output(cs_pin,GPIO.HIGH)
 except:
         GPIO.cleanup()
---
+++ Python/test.py
@@ -0,0 +1,26 @@
+import spidev
+import time
+import os
+
+# open(bus, device) : open(X,Y) will open /dev/spidev-X.Y
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+# Read SPI data from MCP3008, Channel must be an integer 0-7
+def ReadADC(ch):
+    if ((ch > 7) or (ch < 0)):
+       return -1
+    adc = spi.xfer2([1,(8+ch)<<4,0])
+    data = ((adc[1]&3) << 8) + adc[2]
+    return data
+
+delay = 3
+
+try:
+    while True:
+      adcout = ReadADC(6)
+      print (adcout)
+      time.sleep(delay)
+
+except:
+    pass
